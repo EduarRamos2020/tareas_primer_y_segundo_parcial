0fcb8506d4d96a1cc3fa46efc76c260dd2f501b1
EduarRamos2020 <eduar.energissa@gmail.com>
2021-03-10 21:36:15 -0600

    version 1

---
+++ src/help/Honduras.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package help;
+
+/**
+ *
+ * @author Pc
+ */
+public class Honduras extends Hospital {
+
+    @Override
+    public String getpais() {
+       return "Hospital Mario Catarino Rivas";
+    }
+
+    @Override
+   public String getdirector() {
+       return "Director General: Dra Ledy Brizzio";
+    }
+    
+}
