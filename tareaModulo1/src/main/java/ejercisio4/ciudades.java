53a7cf62d335a294276319515d2eaece18f5ed19
EduarRamos2020 <80024911+EduarRamos2020@users.noreply.github.com>
2021-03-10 22:10:32 -0600

    Add files via upload

---
+++ tareaModulo1/src/main/java/ejercisio4/ciudades.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercisio4;
+
+/**
+ *
+ * @autor   EDUAR RAMOS
+ */
+public class ciudades {
+    //Arreglo que mostrara 20 ciudades de paises
+    
+    public static void main(String[]arg){
+         
+        String [] ciudades= new String[20];
+        
+        ciudades[0]= "1- Sao Pablo (BRAZIL)";
+        ciudades[1]= "2- Roma (ITALIA)";
+        ciudades[2]= "3- Ciudad de Mexico (MEXICO)";
+        ciudades[3]= "4- San Pedro Sula (HONDURAS)";
+        ciudades[4]= "5- Managua (NICARAGUA)";
+        ciudades[5]="6- San Jose (COSTA RICA)";
+        ciudades[6]="7- Quito(ECUADOR)";
+        ciudades[7]="8- Shanghai(CHINA)";
+        ciudades[8]="9- Mumbai(INDIA)";
+        ciudades[9]="10- Nueva York(ESTADOS UNIDOS)";
+        ciudades[10]="11- Dehli(INDIA)";
+        ciudades[11]="12- Manila(FILIPINAS)";
+        ciudades[12]="13- Seúl(COREA DEL SUR)";
+        ciudades[13]="14- Beijing(CHINA)";
+        ciudades[14]="15- Dhaka(BANGLADESH)";
+        ciudades[15]="16- Moscú(RUSIA)";
+        ciudades[16]="17- Gran Cairo(EGIPTO)";
+        ciudades[17]="18- Bangkok(TAILANDIA)";
+        ciudades[18]="19- Los Ángeles (ESTADOS UNIDOS)";
+        ciudades[19]="20- Buenos Aires(ARGENTINA)";
+        
+        
+        
+        for(int i=0;i<20;i++){
+            
+            System.out.println(ciudades[i]);
+            
+        }
+        
+        
+         }
+    
+}
---
+++ tareaModulo1/src/main/java/ejercisio5/NumerosPares.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercisio5;
+
+/**
+ *
+ * @author Pc
+ */
+public class NumerosPares {
+     public static void main(String[]arg){
+         System.out.println("numeros pares de 2 hasta 100");
+         
+         
+         for(int i=2;i<=100;i=i+2){
+             System.out.println(i);
+             
+             
+         }
+     }
+}
