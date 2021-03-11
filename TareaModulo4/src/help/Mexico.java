cfb53be3d20ac01e8deefce5e695d4ba098cdff2
EduarRamos2020 <80024911+EduarRamos2020@users.noreply.github.com>
2021-03-10 22:15:16 -0600

    Add files via upload

---
+++ TareaModulo4/src/help/Mexico.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package help;
+
+/**
+ *
+ * @author Pc
+ */
+public class Mexico extends Hospital {
+    
+    public String getpais(){
+        return "Hospital General Dr Eduardo Liceaga";
+    }
+    
+    public String getdirector(){
+        return "Director General: DRA Guadalupe Mercedes Lucía Guerrero Avendaño";
+    }
+}
